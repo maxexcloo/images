75521aa037f1354feb11b489105ceb6f400d7555
Max Schaefer <max@excloo.com>
2024-12-24 13:12:09 +1100

    push changes

--- gatus/run.sh
+++ gatus/run.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-echo "${CONFIG_0_CONTENT}" | base64 -d | gunzip > ${CONFIG_0_PATH}
-export GATUS_CONFIG_PATH=${CONFIG_0_PATH}
 tailscaled --socket /var/run/tailscale/tailscaled.sock --state /var/lib/tailscale/tailscaled.state &
 tailscale up --authkey ${TAILSCALE_TAILNET_KEY} --hostname ${FLY_APP_NAME}-${FLY_MACHINE_ID}
-/app/gatus
+GATUS_CONFIG_PATH=/app/config.yaml /app/gatus
