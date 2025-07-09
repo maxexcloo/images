2bef1025633da1555cd0f33412dbd631ebbd19f3
Max Schaefer <max@excloo.com>
2025-07-09 16:26:13 +0900

    update image

--- gatus/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export GATUS_CONFIG_PATH=/app/config.yaml
-tailscaled --socket /var/run/tailscale/tailscaled.sock --state /var/lib/tailscale/tailscaled.state &
-tailscale up --authkey ${TAILSCALE_TAILNET_KEY} --hostname ${FLY_APP_NAME}-${FLY_MACHINE_ID}
-(while tailscale status; do sleep 30; done; kill 1) &
-exec /app/gatus
